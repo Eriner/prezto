3ee5fe90ad178b0942dc0ba5f5606b0f79debf92
Matt Hamilton <m@tthamilton.com>
2015-11-16 19:36:06 -0500

    Update submodules, change default highlighters.
    Cursor highlighting was fixed in:
    zsh-users/zsh-syntax-highlighting@4f0c293
    
    Revert change removing "brackets" and "cursor" as defaults.
    
    Add sane default highlighters to runcoms/zpreztorc, as
    syntax-highlighting is now enabled by default.

--- modules/syntax-highlighting/init.zsh
+++ modules/syntax-highlighting/init.zsh
@@ -16,7 +16,7 @@ source "${0:h}/external/zsh-syntax-highlighting.zsh" || return 1
 # Set highlighters.
 zstyle -a ':prezto:module:syntax-highlighting' highlighters 'ZSH_HIGHLIGHT_HIGHLIGHTERS'
 if (( ${#ZSH_HIGHLIGHT_HIGHLIGHTERS[@]} == 0 )); then
-  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor)
 fi
 
 # Set highlighting styles.
